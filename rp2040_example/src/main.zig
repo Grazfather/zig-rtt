dff2c084a6667de6b679b93a9878870b3575581a
Hayden Riddiford <hayden@terrakaffe.com>
2024-09-03 15:16:29 -0700

    - Brought back in the UART logger to debug the rtt logger :)

--- rp2040_example/src/main.zig
+++ rp2040_example/src/main.zig
@@ -6,28 +6,34 @@ const time = rp2040.time;
 const Pin = rp2040.gpio.Pin;
 
 pub const microzig_options = .{
-    .logFn = log,
+    .logFn = rp2040.uart.log,
 };
 
-pub fn log(
-    comptime level: std.log.Level,
-    comptime scope: @TypeOf(.EnumLiteral),
-    comptime format: []const u8,
-    args: anytype,
-) void {
-    const level_prefix = comptime "[{}.{:0>6}] " ++ level.asText();
-    const prefix = comptime level_prefix ++ switch (scope) {
-        .default => ": ",
-        else => " (" ++ @tagName(scope) ++ "): ",
-    };
+const gpio = rp2040.gpio;
+const uart = rp2040.uart.num(1);
+const baud_rate = 115200;
+const uart_tx_pin = gpio.num(8);
+const uart_rx_pin = gpio.num(9);
 
-    if (safe_to_use) {
-        const current_time = time.get_time_since_boot();
-        const seconds = current_time.to_us() / std.time.us_per_s;
-        const microseconds = current_time.to_us() % std.time.us_per_s;
-        rtt_instance.up_channels[0].writer().print(prefix ++ format ++ "\r\n", .{ seconds, microseconds } ++ args) catch {};
-    }
-}
+// pub fn log(
+//     comptime level: std.log.Level,
+//     comptime scope: @TypeOf(.EnumLiteral),
+//     comptime format: []const u8,
+//     args: anytype,
+// ) void {
+//     const level_prefix = comptime "[{}.{:0>6}] " ++ level.asText();
+//     const prefix = comptime level_prefix ++ switch (scope) {
+//         .default => ": ",
+//         else => " (" ++ @tagName(scope) ++ "): ",
+//     };
+
+//     if (safe_to_use) {
+//         const current_time = time.get_time_since_boot();
+//         const seconds = current_time.to_us() / std.time.us_per_s;
+//         const microseconds = current_time.to_us() % std.time.us_per_s;
+//         rtt_instance.up_channels[0].writer().print(prefix ++ format ++ "\r\n", .{ seconds, microseconds } ++ args) catch {};
+//     }
+// }
 
 fn blinkLed(led_gpio: *Pin) void {
     led_gpio.put(0);
@@ -60,6 +66,13 @@ fn getLineBlocking(comptime max_line_size: usize, reader: anytype, writer: anyty
 }
 
 pub fn main() !void {
+    uart.apply(.{
+        .baud_rate = baud_rate,
+        .tx_pin = uart_tx_pin,
+        .rx_pin = uart_rx_pin,
+        .clock_config = rp2040.clock_config,
+    });
+    rp2040.uart.init_logger(uart);
 
     // Don't forget to bring a blinky!
     var led_gpio = rp2040.gpio.num(25);
@@ -78,7 +91,7 @@ pub fn main() !void {
         // var line_buffer = try std.BoundedArray(u8, max_line_len).init(0);
         // try getLineBlocking(max_line_len, reader, line_buffer.writer());
         // std.log.info("Got a line: \"{s}\"", .{line_buffer.constSlice()});
-        std.log.info("...take a blink as a reward", .{});
+        // std.log.info("...take a blink as a reward", .{});
         blinkLed(&led_gpio);
     }
 }
