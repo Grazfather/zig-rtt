595a42f4b80a77ffa87619f2a61ddc1b8ff53df1
Hayden Riddiford <hayden@terrakaffe.com>
2024-09-10 10:54:10 -0700

    - Updated example to showcase API better

--- rp2040_example/README.md
+++ rp2040_example/README.md
@@ -9,4 +9,14 @@ This example uses RTT to do the following:
 - Utilizes the `GenericWriter` of channel 0 to have `std.log` output to [RTT Viewer](https://www.segger.com/products/debug-probes/j-link/tools/rtt-viewer/)
 - Loops infinitely waiting to receive a full line from down channel 0, and then logs the received line
 
+Running this firmware with a JLink and RTT Viewer connected the following messages print:
+
+![RTT Viewer Output](images/rtt_viewer_example.png)
+
+And when the buffer information is examined you can see our configuration was successful:
+
+![Buffer Config 1](images/buffer1.png)
+
+![Buffer Config 2](images/buffer2.png)
+
 Note that this example uses the [MicroZig](https://github.com/ZigEmbeddedGroup/microzig) framework for the Raspberry Pi Pico's HAL, however MicroZig is not required to use this package.
--- rp2040_example/src/main.zig
+++ rp2040_example/src/main.zig
@@ -1,41 +1,28 @@
 const std = @import("std");
-const microzig = @import("microzig");
 const rtt = @import("rtt");
+const microzig = @import("microzig");
 const rp2040 = microzig.hal;
 const time = rp2040.time;
 const Pin = rp2040.gpio.Pin;
-
-pub const microzig_options = .{
-    .logFn = rp2040.uart.log,
-};
-
 const gpio = rp2040.gpio;
-const uart = rp2040.uart.num(1);
-const baud_rate = 115200;
-const uart_tx_pin = gpio.num(8);
-const uart_rx_pin = gpio.num(9);
 
-fn blinkLed(led_gpio: *Pin) void {
-    led_gpio.put(0);
-    time.sleep_ms(500);
-    led_gpio.put(1);
-    time.sleep_ms(500);
-}
+/// Dummy example of defining a custom locking/unlocking mechanisms for thread safety
+const pretend_thread_safety = struct {
+    var locked: bool = false;
 
-var pretend_locked: bool = false;
+    const Context = *bool;
 
-const Context = *bool;
+    fn lock(context: Context) void {
+        context.* = true;
+    }
 
-fn pretendLock(context: Context) void {
-    context.* = true;
-}
+    fn unlock(context: Context) void {
+        context.* = false;
+    }
 
-fn pretendUnlock(context: Context) void {
-    context.* = false;
-}
-
-var pretend_lock: rtt.GenericLock(Context, pretendLock, pretendUnlock) = .{
-    .context = &pretend_locked,
+    var generic_lock: rtt.GenericLock(Context, lock, unlock) = .{
+        .context = &locked,
+    };
 };
 
 // Configure RTT with specific sizes/names for up and down channels (2 of each) as
@@ -49,7 +36,7 @@ const rtt_instance = rtt.RTT(.{
         .{ .name = "Terminal", .mode = .BlockIfFull, .buffer_size = 512 },
         .{ .name = "Down2", .mode = .BlockIfFull, .buffer_size = 1024 },
     },
-    .exclusive_access = pretend_lock.any(),
+    .exclusive_access = pretend_thread_safety.generic_lock.any(),
     .linker_section = ".rtt_cb",
 });
 
@@ -59,31 +46,38 @@ const rtt_instance = rtt.RTT(.{
 // Configure RTT with default settings but disable exclusive access protection
 // const rtt_instance = rtt.RTT(.{ .exclusive_access = null });
 
-const Error = error{BufferOverflow};
-fn getLineBlocking(comptime max_line_size: usize, reader: anytype, writer: anytype) !void {
-    var read_buffer: [max_line_size]u8 = undefined;
-    var bytes_read: usize = 0;
-    while (true) {
-        bytes_read += try reader.read(read_buffer[bytes_read..]);
-        if (bytes_read > 0) {
-            if (std.mem.indexOf(u8, read_buffer[0..bytes_read], "\r\n")) |line_end| {
-                return writer.writeAll(read_buffer[0..line_end]);
-            }
-        }
-        if (bytes_read == max_line_size) {
-            return Error.BufferOverflow;
-        }
+// Set up RTT channel 0 as a logger
+
+var rtt_logger: ?rtt_instance.Writer = null;
+
+pub fn log(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_prefix = comptime "[{}.{:0>6}] " ++ level.asText();
+    const prefix = comptime level_prefix ++ switch (scope) {
+        .default => ": ",
+        else => " (" ++ @tagName(scope) ++ "): ",
+    };
+
+    if (rtt_logger) |writer| {
+        const current_time = time.get_time_since_boot();
+        const seconds = current_time.to_us() / std.time.us_per_s;
+        const microseconds = current_time.to_us() % std.time.us_per_s;
+
+        writer.print(prefix ++ format ++ "\r\n", .{ seconds, microseconds } ++ args) catch {};
     }
 }
 
+/// Assigns our custom RTT logging function to MicroZig's log function
+/// A "pub const std_options" decl could be used here instead if not using MicroZig
+pub const microzig_options = .{
+    .logFn = log,
+};
+
 pub fn main() !void {
-    uart.apply(.{
-        .baud_rate = baud_rate,
-        .tx_pin = uart_tx_pin,
-        .rx_pin = uart_rx_pin,
-        .clock_config = rp2040.clock_config,
-    });
-    rp2040.uart.init_logger(uart);
 
     // Don't forget to bring a blinky!
     var led_gpio = rp2040.gpio.num(25);
@@ -93,16 +87,38 @@ pub fn main() !void {
 
     rtt_instance.init();
 
-    const reader = rtt_instance.reader(0);
-    const writer = rtt_instance.writer(0);
+    // Manually write some bytes to RTT up channel 0 so that is shows up in RTT Viewer
+    _ = try rtt_instance.write(0, "Hello RTT!\n");
 
+    // Use std.log instead
+    rtt_logger = rtt_instance.writer(0);
+    std.log.info("Hello from std.log!\n", .{});
+
+    // Now infinitely wait for a complete line, and print it
+    const reader = rtt_instance.reader(0);
+    const max_line_len = 1024;
+    var line_buffer = try std.BoundedArray(u8, max_line_len).init(0);
+    var blink_deadline = time.make_timeout_us(500_000);
+    var led_val: u1 = 0;
     while (true) {
-        const max_line_len = 1024;
-        var line_buffer = try std.BoundedArray(u8, max_line_len).init(0);
-        try getLineBlocking(max_line_len, reader, line_buffer.writer());
+        // Toggle LED every 500 msec
+        if (blink_deadline.is_reached()) {
+            led_gpio.put(led_val);
+            led_val = if (led_val == 0) 1 else 0;
+            blink_deadline = time.make_timeout_us(500_000);
+        }
+
+        // Read some bytes into line buffer, continuing if we get end of stream before our delimiter
+        reader.streamUntilDelimiter(line_buffer.writer(), '\n', line_buffer.unusedCapacitySlice().len) catch |err| switch (err) {
+            error.EndOfStream => continue,
+            error.StreamTooLong => {
+                std.log.err("Line is not allowed to exceed {d} characters, discarding the following data: \"{s}\"", .{ max_line_len, line_buffer.constSlice() });
+                try line_buffer.resize(0);
+                continue;
+            },
+            else => @panic("Unknown error on RTT reader"),
+        };
         std.log.info("Got a line: \"{s}\"", .{line_buffer.constSlice()});
-        std.log.info("...take an RTT blink as a reward", .{});
-        _ = try writer.write("BLINK\n");
-        blinkLed(&led_gpio);
+        try line_buffer.resize(0);
     }
 }
